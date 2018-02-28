89148de3af0eaed0c6ffd01fa1757ad762f96eab
Bitcoin-N <dev@bitcoinn.biz>
2018-02-28 19:13:32 +0100

    Forknote Update

--- src/PaymentGateService/CoinBaseConfiguration.h
+++ src/PaymentGateService/CoinBaseConfiguration.h
@@ -1,36 +1,36 @@
-// Copyright (c) 2015-2016, The Forknote developers
+// Copyright (c) 2015-2016, The Bitcoinnova developers
 //
-// This file is part of Forknote.
+// This file is part of Bitcoinnova.
 //
-// Forknote is free software: you can redistribute it and/or modify
+// Bitcoinnova is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Forknote is distributed in the hope that it will be useful,
+// Bitcoinnova is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with Forknote.  If not, see <http://www.gnu.org/licenses/>.
+// along with Bitcoinnova.  If not, see <http://www.gnu.org/licenses/>.
 
-// Copyright (c) 2016, The Forknote developers
+// Copyright (c) 2016, The Bitcoinnova developers
 //
-// This file is part of Forknote.
+// This file is part of Bitcoinnova.
 //
-// Forknote is free software: you can redistribute it and/or modify
+// Bitcoinnova is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Forknote is distributed in the hope that it will be useful,
+// Bitcoinnova is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with Forknote.  If not, see <http://www.gnu.org/licenses/>.
+// along with Bitcoinnova.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
